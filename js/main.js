25630e3d4aba7383ea423d7f16e557ba0b1796e2
nik-name544 <revenkonik30@gmail.com>
2020-05-16 02:22:45 +0300

    made adaptive

---
+++ js/main.js
@@ -0,0 +1,60 @@
+$(function(){
+
+    $('.slider__inner').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: true,
+        nextArrow: '<img class="slider-arrows__left" src="img/next-arow.svg" alt=""></img>',
+        prevArrow: '<img class="slider-arrows__right" src="img/prev-arow.svg" alt=""></img>',
+        fade: false,
+        asNavFor: '.slider__nav'
+    });
+
+    $('.slider__nav').slick({
+        slidesToShow: 6,
+        slidesToScroll: 1,
+        asNavFor: '.slider__inner',
+        dots: false,
+        arrows: false,
+        // centerMode: false,
+        // focusOnSelect: true,
+        responsive: [
+            {
+              breakpoint: 990,
+              settings: {
+                slidesToShow: 5,
+              }
+            },
+            {
+              breakpoint: 860,
+              settings: {
+                slidesToShow: 4,
+              }
+            },
+            {
+              breakpoint: 680,
+              settings: {
+                slidesToShow: 3,
+              }
+            },
+            {
+              breakpoint: 580,
+              settings: {
+                slidesToShow: 6,
+              }
+            },
+            {
+                breakpoint: 580,
+                settings: {
+                  slidesToShow: 1,
+                }
+              },
+              
+          ]
+    });
+
+    $('.header__menu-btn').on('click', function () {
+        $('.header__menu').slideToggle();
+      });
+
+});
